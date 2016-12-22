895341d350316b987efd8616c12b27ec73a0a6e7
Matt Mink <matthewjmink@gmail.com>
2016-12-22 12:25:35 -0600

    Nav updates

--- app/nav/nav.controller.js
+++ app/nav/nav.controller.js
@@ -9,9 +9,21 @@
         var vm = this;
 
         vm.isActive = isActive;
+        vm.login = login;
 
         function isActive (stateName) {
             return $state.is(stateName);
+        }
+
+        function login () {
+            var ref = new Firebase("https://stevewashere.firebaseio.com");
+            ref.authWithOAuthPopup("google", function(error, authData) {
+                if (error) {
+                    console.log("Login Failed!", error);
+                } else {
+                    console.log("Authenticated successfully with payload:", authData);
+                }
+            });
         }
     }
 })();
